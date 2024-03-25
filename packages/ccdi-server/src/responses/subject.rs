71fae85ec84b6460abf73b7e7cfde705c27cb69c
Clay McLeod <clay.l.mcleod@gmail.com>
2024-03-25 12:04:00 -0500

    revise: updates descriptions of sort order for endpoints

--- packages/ccdi-server/src/responses/subject.rs
+++ packages/ccdi-server/src/responses/subject.rs
@@ -21,13 +21,18 @@ pub struct Subject {
 /// When no sort order is provided, subjects **must** be ordered by the primary
 /// identifier. This means that, when comparing two identifiers:
 ///
-/// 1. The `namespace` field should be sorted alphabetically. If there is a
-///    clear order, return that. Else, if the namespaces are equal,
-/// 2. The `name` field should be sorted alphabetically. Since the `namespace`
-///    and the `name` should always uniquely apply to a single entity, this
-///    should always resolve to an ordering.
+/// 1. The namespace organization field should be sorted alphabetically. If all
+///    values for the namespace organization are equal, continue on to the next
+///    sorting criteria.
+/// 2. The namespace name field should be sorted alphabetically. If all
+///    values for the namespace names are equal, continue on to the next
+///    sorting criteria.
+/// 3. The entity name should be sorted alphabetically.
 ///
-/// Of course, if there is a provided sort order, use that instead.
+/// Since the `namespace` and `name` identifiers should always uniquely apply to
+/// a single entity, this should always resolve to an ordering.
+///
+/// If there is a provided sort order, use that instead.
 #[derive(Debug, Deserialize, Serialize, ToSchema)]
 #[schema(as = responses::Subjects)]
 pub struct Subjects {
