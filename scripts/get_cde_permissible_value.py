70f2669205a32fec3302a3dcf83177867f993a7e
grugna <graglia01@gmail.com>
2024-11-04 11:30:33 -0600

    Update get_cde_permissible_value.py

--- scripts/get_cde_permissible_value.py
+++ scripts/get_cde_permissible_value.py
@@ -12,6 +12,7 @@ url = "https://cadsrapi.cancer.gov/rad/NCIAPI/1.0/api/DataElement/" + cde_id
 enum_name = "LibrarySourceMaterial"
 cde_version = "v1"
 data_type = "sample"
+
 enum_value_template = """
     /// `CCDI_TEMPLATE_REPLACE_1`
     ///
@@ -25,6 +26,13 @@ enum_value_template = """
     CCDI_TEMPLATE_REPLACE_7,
 
 """
+
+enum_display_value_template = """
+            CCDI_TEMPLATE_REPLACE_3::CCDI_TEMPLATE_REPLACE_2 => write!(f, "CCDI_TEMPLATE_REPLACE_1"),"""
+
+enum_distrubution_value_template = """
+            CCDI_TEMPLATE_REPLACE_1 => CCDI_TEMPLATE_REPLACE_2::CCDI_TEMPLATE_REPLACE_3,"""
+
 enum_template = """
 /// **`caDSR CDE CCDI_TEMPLATE_REPLACE_1 CCDI_TEMPLATE_REPLACE_2`**
 ///
@@ -36,6 +44,24 @@ enum_template = """
 #[schema(as = cde::CCDI_TEMPLATE_REPLACE_5::CCDI_TEMPLATE_REPLACE_6::CCDI_TEMPLATE_REPLACE_7)]
 pub enum CCDI_TEMPLATE_REPLACE_7 {
 CCDI_TEMPLATE_REPLACE_8
+}
+
+impl CDE for CCDI_TEMPLATE_REPLACE_7 {}
+
+impl std::fmt::Display for CCDI_TEMPLATE_REPLACE_7 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CCDI_TEMPLATE_REPLACE_9
+        }
+    }
+}
+
+impl Distribution<CCDI_TEMPLATE_REPLACE_7> for Standard {
+    fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> CCDI_TEMPLATE_REPLACE_7 {
+        match rng.gen_range(0..CCDI_TEMPLATE_REPLACE_01) {
+            CCDI_TEMPLATE_REPLACE_02
+        }
+    }
 }
 """
 
@@ -54,8 +80,10 @@ permissible_values = response["DataElement"]["ValueDomain"]["PermissibleValues"]
 
 
 
-
 values = []
+display_values = []
+distribution_values = []
+index = 0
 for permissible_value in permissible_values:
   # print(permissible_value)
   value = copy.deepcopy(enum_value_template)
@@ -70,8 +98,28 @@ for permissible_value in permissible_values:
   value = value.replace("CCDI_TEMPLATE_REPLACE_7", formatted_name)
   values.append(value)
 
+
+  display_value = copy.deepcopy(enum_display_value_template)
+  display_value = display_value.replace("CCDI_TEMPLATE_REPLACE_1", permissible_value["value"])
+  display_value = display_value.replace("CCDI_TEMPLATE_REPLACE_2", formatted_name)
+  display_value = display_value.replace("CCDI_TEMPLATE_REPLACE_3", enum_name)
+  display_values.append(display_value)
+
+  distribution_value = copy.deepcopy(enum_distrubution_value_template)
+  distribution_value = distribution_value.replace("CCDI_TEMPLATE_REPLACE_1", str(index) if index < len(permissible_values) - 1 else "_")
+  distribution_value = distribution_value.replace("CCDI_TEMPLATE_REPLACE_2", enum_name)
+  distribution_value = distribution_value.replace("CCDI_TEMPLATE_REPLACE_3", formatted_name)
+  distribution_values.append(distribution_value)
+
+  index = index + 1
+
 combined_values = ''.join(values)
-# print(combined_values)
+combined_display_values = ''.join(display_values)
+combined_distribution_values = ''.join(distribution_values)
+# print(combined_distribution_values)
+
+
+
 
 cde_enum = copy.deepcopy(enum_template)
 cde_enum = cde_enum.replace("CCDI_TEMPLATE_REPLACE_1", response["DataElement"]["publicId"])
@@ -83,6 +131,11 @@ cde_enum = cde_enum.replace("CCDI_TEMPLATE_REPLACE_5", cde_version)
 cde_enum = cde_enum.replace("CCDI_TEMPLATE_REPLACE_6", data_type)
 cde_enum = cde_enum.replace("CCDI_TEMPLATE_REPLACE_7", enum_name)
 cde_enum = cde_enum.replace("CCDI_TEMPLATE_REPLACE_8", combined_values)
+cde_enum = cde_enum.replace("CCDI_TEMPLATE_REPLACE_9", combined_display_values)
+cde_enum = cde_enum.replace("CCDI_TEMPLATE_REPLACE_01", str(len(permissible_values) - 1))
+cde_enum = cde_enum.replace("CCDI_TEMPLATE_REPLACE_02", combined_distribution_values)
+
+
 
 print(cde_enum)
 
