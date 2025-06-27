be97189cd8239befba268217d3ba5561e3bd7245
asafieva <asafieva@gmail.com>
2025-06-27 17:20:54 -0400

    feat: blog 06-25-2025-the-federation-api-cpi added.

--- docs/.vitepress/theme/components/blog/BlogPosts.vue
+++ docs/.vitepress/theme/components/blog/BlogPosts.vue
@@ -9,11 +9,19 @@
         link="/ccdi-federation-api/blog/09-25-2024-introducing-the-federation-api"
       />
     </ul>
+    <ul>
+      <BlogEntry1
+        title="Federation Resource API with CPI"
+        author="The CCDI Federation Developers"
+        link="/ccdi-federation-api/blog/06-25-2025-the-federation-api-cpi"
+      />
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
 import BlogEntry from "./BlogEntry.vue";
+import BlogEntry1 from "./BlogEntry1.vue";
 </script>
 
 <style scoped></style>
